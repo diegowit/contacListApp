c23f62170332200845a502ca02afcba642a20e88
diego <20098757@mail.wit.ie>
2023-04-29 22:06:25 +0100

    RunMenu() Function

--- src/main/kotlin/utils/Main.kt
+++ src/main/kotlin/utils/Main.kt
@@ -1,9 +1,47 @@
 package utils
+import controller.ContactAPI
+import models.Contact
+import models.Group
+import utils.ScannerInput.readNextChar
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+private val contactAPI = ContactAPI()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() = runMenu()
+
+fun runMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1 -> addContact()
+            2 -> listContacts()
+            3 -> updateContact()
+            4 -> deleteContact()
+            0 -> exitApp()
+            else -> println("Invalid menu choice: $option")
+        }
+    } while (true)
 }
+
+fun mainMenu() = readNextInt(
+    """
+         > -----------------------------------------------------  
+         > |               CONTACT LIST APP                     |
+         > -----------------------------------------------------  
+         > | CONTACT MENU                                      |
+         > |   1) Add a contact                                 |
+         > |   2) List contacts                                 |
+         > |   3) Update a contact                              |
+         > |   4) Delete a contact                              |
+         > -----------------------------------------------------  
+         > |   0) Exit                                          |
+         > -----------------------------------------------------  
+         > ==>> """.trimMargin(">"))
+
+
+fun exitApp() {
+    println("Exiting...bye")
+    exitProcess(0)
+}
+
